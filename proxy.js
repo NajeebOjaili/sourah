b5839f9b99ebefca7ab943442aec9a6295224bbc
NajeebOjaili <najeebojail@gmail.com>
2024-02-10 17:34:42 +0300

    first commit

--- proxy.js
+++ proxy.js
@@ -1,7 +1,7 @@
 const {createProxyMiddleware} = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(
-      '/api',
+      '/http://NajeebOjaili.github.io/sourah',
       createProxyMiddleware({
         target: 'http://localhost:8001',
         changeOrigin: true,
