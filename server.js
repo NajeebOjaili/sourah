95ba8885633c61b33b1eecd0d6d7789fdd777776
NajeebOjaili <najeebojail@gmail.com>
2024-02-10 19:02:16 +0300

    first commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*',(req,res) => {res.sendFile(path.join(__dirname, 'build' , 'index.html'))});
 
-const PORT = process.env.PORT || 3002;
+const PORT =  3002;
 app.use(express.json());
 
 const db = mysql.createConnection({
